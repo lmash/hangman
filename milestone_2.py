1893990b03dcc77bc88285d6d5f20fd6c102df38
mashie <lmachet@yahoo.com>
2023-08-25 16:18:39 +0100

    Refactor user guesses into a function
    Prior to this change, user guess validation was part of the main code
    
    This change isolates guess validation and documents it as well

--- milestone_2.py
+++ milestone_2.py
@@ -2,16 +2,29 @@ import random
 from string import ascii_letters
 
 
-word_list = ['apple', 'pear', 'cherry', 'blackcurrant', 'mango']
+def guess_valid(guess: str) -> bool:
+    """
+    Validates user guess. A valid guess has a length of 1 and
+    is a upper or lowercase letter
+
+    Args:
+        guess (str): User guess
+    Returns:
+        True if guess valid, False if guess invalid
+    """
+    if len(guess) == 1 and guess in ascii_letters:
+        print("Good guess!")
+        return True
+
+    print("Oops! That is not a valid input.")
+    return False
+
+
+word_list = ["apple", "pear", "cherry", "blackcurrant", "mango"]
 print(f"Entire list: {word_list}")
 
 word = random.choice(word_list)
 print(f"Selected word: {word}")
 
 guess = input("Enter a single letter: ")
-
-if len(guess) == 1 and guess in ascii_letters:
-    print("Good guess!")
-else:
-    print("Oops! That is not a valid input.")
-
+guess_valid(guess)
