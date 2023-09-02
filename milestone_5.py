7596e4b198c878d932267d54df4bdeaac95a24bb
mashie <lmachet@yahoo.com>
2023-09-02 10:19:59 +0100

    Apply PEP8 column width of 80
    Prior to this change, code was using 120 characters
    
    This change applies recommended PEP8 column width

--- milestone_5.py
+++ milestone_5.py
@@ -8,13 +8,15 @@ class Hangman:
 
     Attributes:
         word_list (list): a list of words.
-        num_lives (int): the number of lives a player has at the start of the game. Defaults to 5.
+        num_lives (int): the number of lives a player has at the start of the
+        game. Defaults to 5.
     """
     def __init__(self, word_list: List, num_lives: int = 5):
         """See help(Hangman) for accurate signature"""
         self.word = random.choice(word_list).lower()
         self.word_guessed = ['_' for _ in range(len(self.word))]
-        self.num_letters = len(set(self.word))  # Number of UNIQUE letters in the word that have not been guessed yet
+        # Number of UNIQUE letters in the word that have not been guessed yet
+        self.num_letters = len(set(self.word))
         self.num_lives = num_lives
         self.word_list = word_list
         self.list_of_guesses = []
@@ -26,14 +28,15 @@ class Hangman:
         Args:
             guess (str): the guessed letter.
         """
-        # Change to lowercase for check to see if in word, as word is converted to lowercase
+        # Lowercase to check membership in word
         guess_lower = guess.lower()
 
         if guess_lower in self.word:
             print(f"Good guess! {guess} is in the word.")
             for index, letter in enumerate(self.word):
                 if letter == guess_lower:
-                    # guess is added to word_guessed and displayed to the user in the case entered
+                    # guess is added to word_guessed and displayed to the
+                    # user in the case entered
                     self.word_guessed[index] = guess
             self.num_letters -= 1
         else:
@@ -46,12 +49,16 @@ class Hangman:
         return len(guess) == 1 and guess.isalpha()
 
     def ask_for_input(self):
-        """This function is used to ask a user to guess a letter and validate the guess."""
+        """
+        This function is used to ask a user to guess a letter and validate
+        the guess.
+        """
         while True:
             guess = input("Enter a single letter: ")
 
             if not self._valid_guess(guess):
-                print("Invalid letter. Please, enter a single alphabetical character.")
+                print("Invalid letter. Please, enter a single alphabetical "
+                      "character.")
             elif guess.lower() in self.list_of_guesses:
                 print("You already tried that letter!")
             else:
@@ -68,13 +75,17 @@ class Hangman:
         return self.num_lives == 0
 
     def __str__(self) -> str:
-        """This function returns a string representation of correctly guessed letters in the word"""
+        """
+        This function returns a string representation of correctly guessed
+        letters in the word
+        """
         return "".join(self.word_guessed)
 
 
 def play_game(word_list: List):
     """
-    This functions runs the game. It checks to see whether the user needs to enter another letter or has won/lost
+    This functions runs the game. It checks to see whether the user needs to
+    enter another letter or has won/lost
 
     Args:
         word_list (list): A list of words for the hangman game
